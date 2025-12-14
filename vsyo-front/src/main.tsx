e2a63c44669bbffc7a4dfef3605a965d1a78e425
ricardocisco <ricardopenga70@gmail.com>
2025-12-14 19:52:45 -0300

    feat: update biblioteca front-end

--- vsyo-front/src/main.tsx
+++ vsyo-front/src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@mysten/dapp-kit/dist/index.css";
+import "./index.css";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { getFullnodeUrl } from "@mysten/sui/client";
+import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+// Configuração da rede Sui
+const networks = {
+  testnet: { url: getFullnodeUrl("testnet") },
+  mainnet: { url: getFullnodeUrl("mainnet") }
+};
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networks} defaultNetwork="testnet">
+        <WalletProvider autoConnect>
+          <App />
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
